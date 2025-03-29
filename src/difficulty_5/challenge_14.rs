624382a5720b4ea5ce2aff735d25249f4a8b22a3
KaidenSmith <KdntNinja-2010@proton.me>
2025-03-29 11:28:01 +0000

    Add user input functionality to challenges 14 and 15 for traffic light and Olympic values

--- src/difficulty_5/challenge_14.rs
+++ src/difficulty_5/challenge_14.rs
@@ -1,5 +1,13 @@
 use school_challenges_rust::utils::*;
 
 fn main() {
-    println!("Challenge 14");
+    let traffic_light: String = input("Enter the traffic light colour (green, amber, red): ").to_lowercase();
+
+    if traffic_light == "green" {
+        println!("Go.");
+    } else if traffic_light == "amber" {
+        println!("Get Ready.");
+    } else {
+        println!("Stop.");
+    }
 }
--- src/difficulty_5/challenge_15.rs
+++ src/difficulty_5/challenge_15.rs
@@ -1,5 +1,13 @@
 use school_challenges_rust::utils::*;
 
 fn main() {
-    println!("Challenge 15");
+    let olympic_values = ["Respect", "Excellence", "Friendship"];
+
+    let user_input: String = input("Name one of the Olympic Values (Respect, Excellence, Friendship): ");
+
+    if olympic_values.contains(&user_input.as_str()) {
+        println!("That’s correct");
+    } else {
+        println!("Incorrect");
+    }
 }
