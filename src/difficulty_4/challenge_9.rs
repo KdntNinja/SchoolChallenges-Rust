346897e00d76f8fff41b6ebd2121f2374f8a7efe
KaidenSmith <KdntNinja-2010@proton.me>
2025-03-29 11:08:56 +0000

    Add user input functionality to challenge 9 for personalized greeting

--- src/difficulty_4/challenge_9.rs
+++ src/difficulty_4/challenge_9.rs
@@ -1,5 +1,13 @@
 use school_challenges_rust::utils::*;
 
 fn main() {
-    println!("Challenge 9");
+    let stored_name: &str = "Kaiden"; // Randomly chosen first name
+
+    let user_name: String = input("Enter your first name: ");
+
+    if user_name == stored_name {
+        println!("You’re cool.");
+    } else {
+        println!("Nice to meet you.");
+    }
 }
